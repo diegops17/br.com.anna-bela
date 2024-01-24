8b0aa5ea3636678f890fd5e580dd36dc6ccc2f57
diegops17 <psdiego17@gmail.com>
2024-01-23 23:18:44 -0300

    criação da home page do site

--- setup/urls.py
+++ setup/urls.py
@@ -20,5 +20,6 @@ from anna.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('home', home, name='home')
 ]
